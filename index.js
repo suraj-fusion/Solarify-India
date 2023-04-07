31b5e1ae1c7c24a311724d7d5144a2945a696f73
Suraj <surajrajgp@gmail.com>
2023-04-08 00:42:53 +0530

    Initail Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express= require("express");
+const bodyParser=require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+"/public"));
+app.get("/",function(req,res){
+
+    res.render("home");
+
+     
+
+});
+
+///add route
+
+app.listen(3000,function(){
+    console.log("server started on port 3000");
+});
